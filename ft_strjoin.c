70cc94ae7280bb16d3f60683e780fb6c975f971f
Luzog78 <luzog78@gmail.com>
2023-09-30 17:17:10 +0200

    norm

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,18 +6,18 @@
 /*   By: luzog <luzog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 13:43:53 by luzog             #+#    #+#             */
-/*   Updated: 2023/09/28 15:03:40 by luzog            ###   ########.fr       */
+/*   Updated: 2023/09/30 17:15:35 by luzog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int l1;
-	int l2;
-	char *s;
+	int		l1;
+	int		l2;
+	char	*s;
 
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
